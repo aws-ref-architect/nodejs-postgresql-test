c57faca7edfaa179e1b7f9b3a91a909e63f4e2d5
aws-ref-architect <123782877+aws-ref-architect@users.noreply.github.com>
2024-10-21 16:54:29 +0000

    Fix test web app

--- app/server.js
+++ app/server.js
@@ -1,4 +1,7 @@
-const PORT = 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 });
