d9307c31869d39f1a0d27f1c0222be940cf85a39
aws-ref-architect <123782877+aws-ref-architect@users.noreply.github.com>
2024-10-22 01:02:56 +0000

    Add baseline web app with links to test code repos

--- app/frontend/app/src/App.js
+++ app/frontend/app/src/App.js
@@ -3,20 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <main className="App">
+    <main className="App-main">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-header">Github Repositories</h1>
+        <dl className="repo-list"> 
+          <dt className="repo-list-header">Sample apps for web development/Site Reliability Engineering:</dt>
+          <dd className="repo-item">
+            <a className="repo-item-link"
+              href="https://github.com/aws-ref-architect/nodejs-postgresql-test">- CircleCI, NodeJS, PostgreSQL, and React test environments.</a>
+          </dd>
+          <dt className="repo-list-header">Sample Terraform app to host your very own personal cloud:</dt>
+          <dd className="repo-item">
+            <a className="repo-item-link"
+              href="https://github.com/aws-ref-architect/terraform-test">- CircleCI and Terraform definitions for an AWS private cloud</a>
+          </dd>
+        </dl>
       </header>
     </main>
   );
